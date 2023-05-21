a0bd40590c2205cc2453e783fd1f6939af54e492
Oleksii Kasaiev <57752437+AlexeyKasaev3@users.noreply.github.com>
2023-05-21 12:34:09 +0300

    display day if no time selected

--- src/js/index.js
+++ src/js/index.js
@@ -7,22 +7,14 @@ function getText(due) {
     const minutesFormatted = minutes < 10 ? `0${minutes}` : minutes;
     const dayWeek = date.getDay();
     const dayFormatted = dayOfWeek[dayWeek];
+    if(hours === 0 && minutes === 0) return dayFormatted;
     return `${hours}:${minutesFormatted}, ${dayFormatted}`;
-
-}
-
-function wrongTime(due) {
-    if(due === null) return true;
-    const date = new Date(due);
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    return hours === 0 && minutes === 0;
 }
 
 window.TrelloPowerUp.initialize({
     'card-badges': function (t, opts) {
         return t.card('all').then(function (card) {
-            if(wrongTime(card.due)) return [];
+            if(!card.due) return [];
             return [{
                 text: getText(card.due)
             }];
