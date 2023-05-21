05069ac877ba073a4c5f1524481420f565ba1dde
Oleksii Kasaiev <57752437+AlexeyKasaev3@users.noreply.github.com>
2023-05-21 12:28:18 +0300

    add functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,31 @@
+const dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+function getText(due) {
+    const date = new Date(due)
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const minutesFormatted = minutes < 10 ? `0${minutes}` : minutes;
+    const dayWeek = date.getDay();
+    const dayFormatted = dayWeek[dayWeek];
+    return `${hours}:${minutesFormatted}, ${dayFormatted};`
+
+}
+
+function wrongTime(due) {
+    if(due === null) return true;
+    const date = new Date(due);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    return hours === 0 && minutes === 0;
+}
+
 window.TrelloPowerUp.initialize({
     'card-badges': function (t, opts) {
         return t.card('all').then(function (card) {
-            console.log(card);
-            return [];
+            if(wrongTime(card.due)) return [];
+            return [{
+                text: getText(card.due)
+            }];
         });
     }
 })
