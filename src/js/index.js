64449244ce2bee091b1eec0b4f17c9d3a7547f60
Oleksii Kasaiev <57752437+AlexeyKasaev3@users.noreply.github.com>
2023-05-21 19:49:20 +0300

    add due date number badge

--- src/js/index.js
+++ src/js/index.js
@@ -2,23 +2,35 @@ const dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Frid
 
 function getBadge(due) {
     const date = new Date(due);
-    const today = new Date();
-    const difference = date.getTime() - today.getTime();
-    const daysDifference = Math.ceil(difference / (1000 * 3600 * 24));
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const minutesFormatted = minutes < 10 ? `0${minutes}` : minutes;
     const dayWeek = date.getDay();
     const dayFormatted = dayOfWeek[dayWeek];
-    if(hours === 0 && minutes === 0) return {text: `${dayFormatted}, ${daysDifference}d`};
-    return {text: `${hours}:${minutesFormatted}, ${dayFormatted}, ${daysDifference}d`, color: 'red'};
+    if(hours === 0 && minutes === 0) return {text: `${dayFormatted}`};
+    return {text: `${hours}:${minutesFormatted}, ${dayFormatted}`, color: 'red'};
+}
+
+function getDaysLeftBadge(due) {
+    const date = new Date(due);
+    const today = new Date();
+    const difference = date.getTime() - today.getTime();
+
+    let badgeColor;
+    if(difference <= 14) badgeColor = 'yellow';
+    if(difference <= 7) badgeColor = 'orange';
+    if(difference === 0) badgeColor = 'red';
+    if(difference < 0) badgeColor = 'light-gray';
+
+    const daysDifference = Math.ceil(difference / (1000 * 3600 * 24));
+    const displayedText = difference === 0 ? 'Today' : `${daysDifference}d`
 }
 
 window.TrelloPowerUp.initialize({
     'card-badges': function (t, opts) {
         return t.card('all').then(function (card) {
             if(!card.due) return [];
-            return [getBadge(card.due)];
+            return [getBadge(card.due), getDaysLeftBadge(card.due)];
         });
     }
 })
