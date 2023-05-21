93439b40bcf5543841f488f8e9e552ee4bbb8556
Oleksii Kasaiev <57752437+AlexeyKasaev3@users.noreply.github.com>
2023-05-21 12:43:15 +0300

    make badge red if has time

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,21 @@
 const dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-function getText(due) {
+function getBadge(due) {
     const date = new Date(due);
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const minutesFormatted = minutes < 10 ? `0${minutes}` : minutes;
     const dayWeek = date.getDay();
     const dayFormatted = dayOfWeek[dayWeek];
-    if(hours === 0 && minutes === 0) return dayFormatted;
-    return `${hours}:${minutesFormatted}, ${dayFormatted}`;
+    if(hours === 0 && minutes === 0) return {text: dayFormatted};
+    return {text: `${hours}:${minutesFormatted}, ${dayFormatted}`, color: 'red'};
 }
 
 window.TrelloPowerUp.initialize({
     'card-badges': function (t, opts) {
         return t.card('all').then(function (card) {
             if(!card.due) return [];
-            return [{
-                text: getText(card.due)
-            }];
+            return [getBadge(card.due)];
         });
     }
 })
